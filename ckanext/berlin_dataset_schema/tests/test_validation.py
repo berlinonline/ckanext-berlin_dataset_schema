e202e0dce571172648383637867c3f3802777c95
Knud Möller <knud@datalysator.com>
2018-10-05 14:53:01 +0200

    structure validation test into classes

--- ckanext/berlin_dataset_schema/tests/test_validation.py
+++ ckanext/berlin_dataset_schema/tests/test_validation.py
@@ -3,8 +3,10 @@
 
 from datetime import datetime
 from nose.tools import raises
-import ckanext.berlin_dataset_schema.validation as validation
 import ckan.lib.navl.dictization_functions as df
+import ckan.tests.factories as factories
+import ckan.tests.helpers as helpers
+import ckanext.berlin_dataset_schema.validation as validation
 
 # workaround for missing translator object from 
 # https://github.com/ckan/ckanext-dcat/commit/bd490115da8087a14b9a2ef603328e69535144bb
@@ -15,33 +17,84 @@ registry.prepare()
 from pylons import translator
 registry.register(translator, MockTranslator())
 
-def test_isodate_notime_empty_is_none():
-    assert validation.isodate_notime('') is None
+class TestIsodateNotime:
+    """
+    Tests for validation.isodate_notime() validator.
+    """
 
-def test_isodate_notime_datetime_object_is_truncated():    
-    assert validation.isodate_notime(datetime(1969, 9, 6, 7, 43, 23, 10)) == "1969-09-06"
+    def test_isodate_notime_empty_is_none(self):
+        assert validation.isodate_notime('') is None
 
-def test_isodate_notime_datetime_string_is_truncated():
-    assert validation.isodate_notime("1969-09-06T07:43:23.10") == "1969-09-06"
+    def test_isodate_notime_datetime_object_is_truncated(self):    
+        assert validation.isodate_notime(datetime(1969, 9, 6, 7, 43, 23, 10)) == "1969-09-06"
 
-def test_isodate_notime_illegal_string_after_date_is_ignored():
-    assert validation.isodate_notime("1969-09-06foo bar") == "1969-09-06"
+    def test_isodate_notime_datetime_string_is_truncated(self):
+        assert validation.isodate_notime("1969-09-06T07:43:23.10") == "1969-09-06"
 
-@raises(df.Invalid)
-def test_isodate_notime_datetime_illegal_date_string_raises_invalid_error():
-    validation.isodate_notime("foor bar")
+    def test_isodate_notime_illegal_string_after_date_is_ignored(self):
+        assert validation.isodate_notime("1969-09-06foo bar") == "1969-09-06"
 
-@raises(ValueError)
-def test_isodate_notime_datetime_date_before_1900_raises_value_error():
-    validation.isodate_notime(datetime(1869, 9, 6, 7, 43, 23, 10))
+    @raises(df.Invalid)
+    def test_isodate_notime_datetime_illegal_date_string_raises_invalid_error(self):
+        validation.isodate_notime("foor bar")
+
+    @raises(ValueError)
+    def test_isodate_notime_datetime_date_before_1900_raises_value_error(self):
+        validation.isodate_notime(datetime(1869, 9, 6, 7, 43, 23, 10))
 
 # -------------------
 
-@raises(df.Invalid)
-def test_is_berlin_type_raises_invalid_error_for_bad_value():
-    validation.is_berlin_type('goo star')
+class TestIsBerlinType:
+    """
+    Tests for validation.is_berlin_type() validator.
+    """
 
-def test_is_berlin_type_gives_correct_answer():
-    berlin_types = [ 'datensatz', 'dokument', 'app' ]
-    for _type in berlin_types:
-        assert validation.is_berlin_type(_type) is _type
+    @raises(df.Invalid)
+    def test_is_berlin_type_raises_invalid_error_for_bad_value(self):
+        validation.is_berlin_type('goo star')
+
+    def test_is_berlin_type_gives_correct_answer(self):
+        berlin_types = [ 'datensatz', 'dokument', 'app' ]
+        for _type in berlin_types:
+            assert validation.is_berlin_type(_type) is _type
+
+# -------------------
+
+class TestIsGroupNameValid:
+    """
+    Tests for validation.is_group_name_valid() check.
+    """
+
+    def setup(self):
+        helpers.reset_db()
+        self.groups = []
+        group_names = [
+            u'arbeit',
+            u'bildung',
+            u'demographie',
+            u'erholung',
+            u'geo',
+            u'verkehr',
+            u'verwaltung'
+            u'wahl',
+            u'wirtschaft',
+            u'wohnen',
+        ]
+        for group_name in group_names:
+            self.groups.append(factories.Group(name=group_name))
+        self.user = factories.User()
+        for group in self.groups:
+            helpers.call_action(
+                'member_create',
+                id=group['id'],
+                object=self.user['id'],
+                object_type='user',
+                capacity='editor'
+            )
+
+    def test_empty_is_an_invalid_group_name(self):
+        assert validation.is_group_name_valid('empty', { 'user': self.user['name'] }) is False
+
+    def test_group_names_are_valid_group_names(self):
+        for group in self.groups:
+            assert validation.is_group_name_valid(group['name'], { 'user': self.user['name'] }) is True
