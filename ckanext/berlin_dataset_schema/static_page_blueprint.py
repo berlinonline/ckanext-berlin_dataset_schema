12bc666c86b1e6c025f0a3452d9c90cfb53b3381
Jovanka <jovanka.gulicoska@berlinonline.de>
2023-11-01 10:42:44 +0100

    Add new route so that schema is available also under /schema/ Bring back the /schema/index.html so that it also exists

--- ckanext/berlin_dataset_schema/static_page_blueprint.py
+++ ckanext/berlin_dataset_schema/static_page_blueprint.py
@@ -10,3 +10,4 @@ def schema():
 
 static_page_blueprint = Blueprint('static_page_blueprint', __name__)
 static_page_blueprint.add_url_rule(u'/schema', methods=[u'GET'], view_func=schema)
+static_page_blueprint.add_url_rule(u'/schema/', methods=[u'GET'], view_func=schema)
